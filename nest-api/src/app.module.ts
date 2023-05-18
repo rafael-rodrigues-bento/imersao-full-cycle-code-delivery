802fc64d5f7e0777a3809dd11259dbbe6f81eed6
rafael <rafael.rodrigues.bento@outlook.com>
2023-05-18 00:32:07 -0300

    finalizada aula 03

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-
+//ES7 decorators
 @Module({
   imports: [
     ConfigModule.forRoot(),
--- nest-api/src/main.ts
+++ nest-api/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.connectMicroservice({
     transport: Transport.KAFKA,
